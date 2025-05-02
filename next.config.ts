5de1676397ae891e6392d974bbd7773a1063ce20
simon <simonculus@gmail.com>
2025-05-03 01:48:10 +0200

    Feat/docs (#4)
    * feat: add protocols page with Google Drive integration
    
    * feat: add statutes
    
    * fix github actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
+import remarkGfm from "remark-gfm";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+  pageExtensions: ["mdx", "tsx", "ts", "jsx", "js"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+  },
+});
+
+export default withMDX(nextConfig);

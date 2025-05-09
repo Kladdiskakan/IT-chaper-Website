2be8d726cfda968a985eceb5554ccfad8d99a690
simon <simonculus@gmail.com>
2025-05-09 15:58:13 +0200

    feat: add statute table of contents for mobile

--- app/documents/statutes/md-wrapper.tsx
+++ app/documents/statutes/md-wrapper.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import useClosestAnchor from "@/hooks/use-closest-anchor";
 import { getHeadings } from "@/lib/md";
 import { cn } from "@/lib/utils";
-import { ScrollIcon } from "lucide-react";
+import { ScrollIcon, TableOfContentsIcon } from "lucide-react";
 import Link from "next/link";
 
 const MdWrapper = ({
@@ -27,6 +34,36 @@ const MdWrapper = ({
           documents.
         </p>
       </div>
+      <Sheet>
+        <SheetTrigger className="flex shadow-xs items-center justify-center lg:hidden fixed top-6 right-6 size-9 bg-background rounded-md border">
+          <TableOfContentsIcon className="size-4 text-primary" />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader className="mb-0">
+            <SheetTitle>Table of Contents</SheetTitle>
+          </SheetHeader>
+          <div className="px-6 overflow-y-auto -mt-6 pb-18">
+            <ul>
+              {headings?.slice(1).map((heading) => (
+                <li
+                  style={{ marginLeft: `${heading.level * 1.2}rem` }}
+                  key={heading.link}
+                >
+                  <Link
+                    className={cn(
+                      "font-poppins font-medium text-sm hover:opacity-80 transition-all",
+                      anchor === heading.link.slice(1) && "text-primary",
+                    )}
+                    href={heading.link}
+                  >
+                    {heading.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </SheetContent>
+      </Sheet>
       <div className="flex">
         <div>
           <div className="hidden lg:block sticky top-[calc(4rem+1px)] w-[280px]">

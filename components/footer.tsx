ddefaec8a3b1b6409cc4fd591957cd2337e610e2
simon <simonculus@gmail.com>
2025-05-09 14:31:17 +0200

    add mobile navbar + mobile styling fixes (#6)
    * add mobile navbar + mobile styling fixes
    
    * fix course browser url

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,8 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="border-t">
-      <div className="container mx-auto p-6 border-x">
+    <footer className="border-t mb-24 sm:mb-0 h-50">
+      <div className="container mx-auto h-full p-6 sm:border-x">
         <div className="mb-8">
           <ul className="text-sm space-y-2">
             <li>
@@ -42,14 +42,14 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center flex-col sm:flex-row gap-4">
           <p className="text-muted-foreground text-xs">
             {`Copyright © ${currentYear} Sektionen för Informationsteknik`}
           </p>
           <p className="text-muted-foreground text-xs text-right">
             The source code for this site is available on{" "}
             <Link
-              className="hover:underline underline-offset-4"
+              className="underline sm:no-underline hover:underline underline-offset-4"
               href="https://www.github.com/itsektionen"
             >
               GitHub

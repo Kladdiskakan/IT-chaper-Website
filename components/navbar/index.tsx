4d61cb10578b4dca51b9911dff8cf40c9b1fdc7d
simon <simonculus@gmail.com>
2025-06-18 23:44:22 +0200

    use next.js link instead of html a

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -122,25 +122,25 @@ const Navbar = () => {
 export { Navbar };
 
 const ListItem = React.forwardRef<
-  React.ComponentRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
+  React.ComponentRef<typeof Link>,
+  React.ComponentPropsWithoutRef<typeof Link>
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
           ref={ref}
           className={cn(
             "block select-none flex-col items-start space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className,
           )}
-          {...props}
+          href={props.href}
         >
           <div className="text-sm font-medium leading-none">{title}</div>
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   );

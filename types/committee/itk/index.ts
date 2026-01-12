5328f89b61fed8486bc511dd439941ce15e254d0
Leonard Lindgren <65599046+lindgrenleonard@users.noreply.github.com>
2026-01-12 18:19:24 +0100

    misc: integrate ITK & TMEIT events (#35)
    Co-authored-by: Hugo <54435874+Cactooz@users.noreply.github.com>

---
+++ types/committee/itk/index.ts
@@ -0,0 +1,23 @@
+import { CalendarEvent } from "@/lib/events";
+
+export type ItkEvent = {
+  id: string;
+  title: string;
+  type: string;
+  imageUrl: string;
+  start: string;
+  end: string;
+  description: string;
+  location?: string;
+};
+
+export const mapItkEvent = (itkEvent: ItkEvent): CalendarEvent => ({
+  id: `itk-${itkEvent.id}`,
+  title: itkEvent.title,
+  description: itkEvent.description || undefined,
+  start: new Date(itkEvent.start),
+  end: new Date(itkEvent.end),
+  location: itkEvent.location,
+  imageUrl: "/assets/img/events/itk.webp",
+  committeeSlug: "itk",
+});

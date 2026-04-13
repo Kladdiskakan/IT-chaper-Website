2492c3c0e983774a659a99a0366c1c2d96c84c93
Kevin <kevin@gregard.se>
2026-04-13 22:41:19 +0200

    removed some css

--- app/[locale]/companies/company-contact-form.tsx
+++ app/[locale]/companies/company-contact-form.tsx
@@ -136,7 +136,7 @@ return (
             <FormControl>
               <textarea
                 {...field}
-                className="min-h-[80px]border-input w-full rounded-md border bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
+                className="min-h-[80px] w-full rounded-md border px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
                 placeholder={t('CompanyForm.messagePlaceholder')}
                 rows={4}
               />
